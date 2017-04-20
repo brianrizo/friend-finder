db5949f47ef65059ee58148e685ef09b64d9442c
Brian Rizo <codingbr@gmail.com>
2017-04-19 21:44:51 -0400

    new data

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -8,48 +8,48 @@ var friendsArray = [
   "name":"Brian",
   "photo":"https://pbs.twimg.com/profile_images/803949920281567232/BVFsbUZt.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
 },
 {
 "name": "Mila",
 "photo": "http://media.ellentv.com/2014/07/07/mila-kunis-episode-480x360.jpg",
 "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
 },
 {
 "name": "David",
 "photo": "https://pbs.twimg.com/profile_images/646374589916778496/i53D1Q35.jpg",
 "scores":[
-      3,
-      2,
-      1,
-      1,
-      5,
-      3,
-      2,
-      3,
-      5,
-      4
+      "3",
+      "4",
+      "1",
+      "2",
+      "5",
+      "2",
+      "3",
+      "5",
+      "4",
+      "1"
     ]
 }
 
